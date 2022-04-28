c36d10b703df9364cd10dfc4220bd3a5c2c26e21
londonwafflez <93176330+londonwafflez@users.noreply.github.com>
2022-04-27 21:40:02 -0700

    Starting movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,39 +4,53 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Sprite Up;
+    /*public Sprite Up;
     public Sprite Down;
     public Sprite Right;
     public Sprite Left;
-    public float speed;
+    public float speed;*/
 
+    public float speed = 500;
+    public Transform obj;
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        //Vector3 move;
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            GetComponent<SpriteRenderer>().sprite = Up;
-            //move = new Vector3(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            GetComponent<SpriteRenderer>().sprite = Left;
-            //move = new Vector3(0, 0, speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            GetComponent<SpriteRenderer>().sprite = Right;
-            //move = new Vector3(speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            GetComponent<SpriteRenderer>().sprite = Down;
-            // move = new Vector3(0, 0, -speed * Time.deltaTime);
-        }
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
 
-        //transform.position += move;
+        obj.transform.position += tempVect;
     }
+
+    /*   // Update is called once per frame
+       void Update()
+       {
+           Vector3 move;
+
+           if (Input.GetKey(KeyCode.W))
+           {
+               GetComponent<SpriteRenderer>().sprite = Up;
+               move = new Vector3(-speed * Time.deltaTime, 0, 0);
+           }
+           if (Input.GetKey(KeyCode.A))
+           {
+               GetComponent<SpriteRenderer>().sprite = Left;
+               move = new Vector3(0, 0, speed * Time.deltaTime);
+           }
+           if (Input.GetKey(KeyCode.D))
+           {
+               GetComponent<SpriteRenderer>().sprite = Right;
+               move = new Vector3(speed * Time.deltaTime, 0, 0);
+           }
+           if (Input.GetKey(KeyCode.S))
+           {
+               GetComponent<SpriteRenderer>().sprite = Down;
+               move = new Vector3(0, 0, -speed * Time.deltaTime);
+           }
+
+           transform.position += move;
+       }
+    */
 }
