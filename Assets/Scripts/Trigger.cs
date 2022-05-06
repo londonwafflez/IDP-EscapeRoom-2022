28ffa8376cb5cfcc6898da3a84cc98c93d6eb42d
Isaiah-Arreola <104409300+Isaiah-Arreola@users.noreply.github.com>
2022-05-06 15:12:42 -0700

    Trigger Script changed and storage room sensors changed

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
     public GameObject obj;
+    bool isActive = false;
+
+    IEnumerator Wait() 
+    {
+
+        yield return 24;
+    }
+
 
     void OnTriggerStay2D(Collider2D other)
     {
         if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.Space))
         {
-            obj.SetActive(true); // false to hide, true to show
+            obj.SetActive(!isActive); // false to hide, true to show
+            isActive = !isActive;
+            StartCoroutine(Wait());
         }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         obj.SetActive(false);
+        isActive = false;
+
     }
 }
