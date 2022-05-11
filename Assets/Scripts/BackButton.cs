15bd48ddd97112f382abfd8af7edd3c94360bb6c
Cramirez2435 <104409310+Cramirez2435@users.noreply.github.com>
2022-05-10 23:05:02 -0800

    Functioning leaderboard and control buttons
    I was able to make the leaderboard and controls buttons to work, but I made them separate scenes. I also added a functioning back button to each scene.

---
+++ Assets/Scripts/BackButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Scene3()
+    {
+        SceneManager.LoadScene("");
+    }
+    public void exitgame()
+    {
+        Debug.Log("exitgame");
+        Application.Quit();
+    }
+
+    /*void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.Space))
+        {
+            var nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                string nextSceneName = GetSceneNameByBuildIndex(nextSceneIndex);
+                SceneManager.LoadScene(nextSceneName);
+            }
+            else
+            {
+                Debug.Log("Next scene unavailable");
+            }
+        }
+    }*/
+}
+
