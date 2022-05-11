9ee9954e6eea2101684b3c22bb010469040fb5bd
Isaiah-Arreola <104409300+Isaiah-Arreola@users.noreply.github.com>
2022-05-10 20:46:42 -0700

    Added the enlarged version of code screen
    I added the code screen where we will input a password to open a crate. The code works so that there will be an output. I also coded the textbox to change depending on the numbers u put

---
+++ Assets/Scripts/Passcode.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Passcode : MonoBehaviour
+{
+    string Code = "1234";
+    string Nr = null;
+    int NrIndex = 0;
+    string alpha;
+    public Text UiText = null;
+
+    public void CodeFunction(string Numbers)
+    {
+        NrIndex++;
+        Nr = Nr + Numbers;
+        UiText.text = Nr;
+    }
+
+    public void Enter()
+    {
+        if (Nr == Code)
+        {
+            Debug.Log("Correct");
+        }
+
+        if (Nr != Code)
+        {
+            Debug.Log("Incorrect");
+            NrIndex++;
+            Nr = null;
+            UiText.text = Nr;
+        }
+        
+    }
+
+    public void Delete()
+    {
+        NrIndex++;
+        Nr = null;
+        UiText.text = Nr;  
+        Debug.Log("Cleared");
+    }
+}
