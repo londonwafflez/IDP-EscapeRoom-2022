76818937a4c809c4fc0216d6f4a23a20293035ae
Cramirez2435 <104409310+Cramirez2435@users.noreply.github.com>
2022-05-24 15:13:11 -0800

    Worked on leaderboard
    Leaderboard is almost done.

---
+++ Assets/Scripts/Leaderboard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Data : MonoBehaviour
+{
+    [SerializeField] string Username;
+    [SerializeField] string Score;
+    private string URL = "https://docs.google.com/spreadsheets/d/1ZlVADmM7B8WjgOY-8yd7nzJUorSmVfBua0TbrFYvv5Q/edit?resourcekey#gid=493063165"
+    
+    void Awake()
+
+    public void Send()
+    {
+        startCoroutine(Post(Username, Score));
+    }
+
+    iEnumerator Post(string s1, string s2, string s3, string s4, string s5, string s6,string s7, string s8, string s9, string s10, string s11, string s12, string s13, string s14, string s15);
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("",s1);
+        form.AddField("",s2);
+    
+
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+    }
+}
