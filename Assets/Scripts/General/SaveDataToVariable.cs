3c57547d1a239d6fd193718b3625d0528b55aecb
Cramirez2435 <104409310+Cramirez2435@users.noreply.github.com>
2022-05-27 00:14:18 -0800

    Stored Some Input Fields as a variable

---
+++ Assets/Scripts/General/SaveDataToVariable.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveDataToVariable : MonoBehaviour
+{
+    private string input;
+    private string input1;
+    private string input2;
+    private string input3;
+    private string input4;
+    private string input5;
+    private string input6;
+    private string input7;
+    private string input8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReadStringInput(string s)
+    {
+        input = s;
+        Debug.Log(input);
+    }
+    public void ReadStringInput1(string s)
+    {
+        input = s;
+        Debug.Log(input1);
+    }
+    public void ReadStringInput2(string s)
+    {
+        input = s;
+        Debug.Log(input2);
+    }
+    public void ReadStringInput3(string s)
+    {
+        input = s;
+        Debug.Log(input3);
+    }
+    public void ReadStringInput4(string s)
+    {
+        input = s;
+        Debug.Log(input4);
+    }
+    public void ReadStringInput5(string s)
+    {
+        input = s;
+        Debug.Log(input5);
+    }
+    public void ReadStringInput6(string s)
+    {
+        input = s;
+        Debug.Log(input6);
+    }
+    public void ReadStringInput7(string s)
+    {
+        input = s;
+        Debug.Log(input7);
+    }
+    public void ReadStringInput8(string s)
+    {
+        input = s;
+        Debug.Log(input8);
+    }
+   
+   
+}
